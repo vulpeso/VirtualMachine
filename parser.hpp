b2a05cee2a2a8938035395bf58c604e26dca74e1
Minified <gora.pub@gmail.com>
2017-02-15 10:29:00 +0100

    array based command parsing

--- parser.hpp
+++ parser.hpp
@@ -11,7 +11,7 @@
 extern bool DEBUG;
 
 
-/* abstrakcyjna klasa definiuj¹ca zachowanie zadañ */
+/* abstrakcyjna klasa definiujï¿½ca zachowanie zadaï¿½ */
 
 class Parser{
 
@@ -19,7 +19,7 @@ private:
 	Parser(){}
 	
 public:
-	/* rozpoznanie bie¿¹cej instrukcji */
+	/* rozpoznanie bieï¿½ï¿½cej instrukcji */
 	
 	static void readFile( std::vector<int>& code, std::string filename)
 	{
@@ -29,44 +29,34 @@ public:
 	    if (!file.is_open()) return;
 		if(DEBUG) printf("Parsing\n");
 	    std::string word;
+	    static int commands_count=17;
+		std::string commands[commands_count] = {
+			"halt",
+			"start",
+			"add",
+			"sub",
+			"loadi",
+			"mv",
+			"label",
+			"jmp",
+			"jmpz",
+			"jmpf",
+			"rcv",
+			"snd",
+			"spawn",
+			"exit",
+			"abort",
+			"print",
+			"join"
+		};
 	    while (file >> word)
 	    {
 	    		if(DEBUG)
 	    			printf("\tno. %2d \t word: %s ", code.size(), word.c_str());
-	    		if(word == "halt")
-	    			code.push_back(0);
-	    		else if(word == "start")
-	    			code.push_back(1);
-	    		else if(word == "add")
-	    			code.push_back(2);
-	    		else if(word == "sub")
-	    			code.push_back(3);
-	    		else if(word == "loadi")
-	    			code.push_back(4);
-	    		else if(word == "mv")
-	    			code.push_back(5);
-	    		else if(word == "label")
-	    			code.push_back(6);
-	    		else if(word == "jmp")
-	    			code.push_back(7);
-	    		else if(word == "jmpz")
-	    			code.push_back(8);
-	    		else if(word == "jmpf")
-	    			code.push_back(9);
-	    		else if(word == "rcv")
-	    			code.push_back(10);
-	    		else if(word == "snd")
-	    			code.push_back(11);
-	    		else if(word == "spawn")
-	    			code.push_back(12);
-	    		else if(word == "exit")
-	    			code.push_back(13);
-	    		else if(word == "abort")
-	    			code.push_back(14);
-	    		else if(word == "print")
-	    			code.push_back(15);
-	    		else if(word == "join")
-	    			code.push_back(16);
+	    		int index;
+	    		for(index = 0; index < commands_count && commands[index] != word; index++);
+	    		if(index<commands_count)
+					code.push_back(index);
 	    		else 
 	    			code.push_back(strtol(word.c_str(), NULL, 0));
 	    		if(DEBUG)
